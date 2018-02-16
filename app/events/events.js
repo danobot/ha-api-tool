09fc94e5423cda06626178f379ba3b2b083be1c4
Daniel Mason <danielmason@iinet.net.au>
2018-02-16 23:57:50 +0800

    added status bar and moved connection message down there

--- app/events/events.js
+++ app/events/events.js
@@ -7,41 +7,41 @@ app.config(['$routeProvider', function($routeProvider) {
     templateUrl: 'events/events.html',
     controller: 'EventsCtrl'
   });
-}]).controller('EventsCtrl', ['$scope','$http', 'hass','CredentialService', function($scope, $http, hass, CredentialService) {
-  $scope.domains = ['light','group','scene','automation','media_player','script'];
+}]).controller('EventsCtrl', ['$scope', '$http', 'hass', 'CredentialService', function($scope, $http, hass, CredentialService) {
+  $scope.domains = ['light', 'group', 'scene', 'automation', 'media_player', 'script'];
   connect();
-  CredentialService.subscribe(connect );
+  CredentialService.subscribe(connect);
   $scope.events = [];
   $scope.eventCount = 0;
   const status = document.getElementById('status');
   const root = document.getElementById('content');
   $scope.selection;
 
-  function eventCallback(events){
+  function eventCallback(events) {
     console.log("New Event", events);
     $scope.events.push(events);
     $scope.eventCount = $scope.eventCount++;
     $scope.$apply();
   }
-  function connect()
-  {
+  function connect() {
 
-    HAWS.createConnection('wss://'+CredentialService.getApiUrl()+'/websocket',CredentialService.getHeaders()).then(conn => {
-      status.innerHTML = 'yes';
+    HAWS.createConnection('wss://' + CredentialService.getApiUrl() + '/websocket', CredentialService.getHeaders()).then(conn => {
+      status.innerHTML = 'Connected';
       conn.subscribeEvents(eventCallback);
-    }, err => { status.innerHTML = `Error (Check Credentials)`; });
+    }, err => {
+      status.innerHTML = `Connection Error (Check Credentials)`;
+    });
 
   }
 
-  $scope.select = function(item, index)
-  {
-    $scope.selection =  item;
+  $scope.select = function(item, index) {
+    $scope.selection = item;
     $scope.selectionId = index;
     // $scope.selection_json = JSON.stringify(item,'\n',4);
-    console.log("Item:",item);
-    console.log("Index:",index);
-    console.log("selection:",$scope.selection);
-    // tableform.$setPristine();
+    console.log("Item:", item);
+    console.log("Index:", index);
+    console.log("selection:", $scope.selection);
+  // tableform.$setPristine();
   }
 
 
--- app/ha-services/services.js
+++ app/ha-services/services.js
@@ -7,8 +7,8 @@ app.config(['$routeProvider', function($routeProvider) {
     templateUrl: 'ha-services/services.html',
     controller: 'ServicesCtrl'
   });
-}]).controller('ServicesCtrl', ['$scope','$http', 'hass', 'CredentialService', function($scope, $http, hass, CredentialService) {
-  $scope.domains = ['light','group','scene','automation','media_player','script'];
+}]).controller('ServicesCtrl', ['$scope', '$http', 'hass', 'CredentialService', function($scope, $http, hass, CredentialService) {
+  $scope.domains = ['light', 'group', 'scene', 'automation', 'media_player', 'script'];
 
   $scope.service = [];
   CredentialService.subscribe(connect);
@@ -19,28 +19,29 @@ app.config(['$routeProvider', function($routeProvider) {
 
   function connect() {
 
-    HAWS.createConnection('wss://'+CredentialService.getApiUrl()+'/websocket',CredentialService.getHeaders()).then(conn => {
-      status.innerHTML = 'yes';
-      conn.getServices().then(  function serviceCallback(services){
-          console.log("New Event", services);
-          $scope.services = Object.assign(services);
-          $scope.$apply();
-        });
-    }, err => { status.innerHTML = `no (${err})`; });
+    HAWS.createConnection('wss://' + CredentialService.getApiUrl() + '/websocket', CredentialService.getHeaders()).then(conn => {
+      status.innerHTML = 'Connected';
+      conn.getServices().then(function serviceCallback(services) {
+        console.log("New Event", services);
+        $scope.services = Object.assign(services);
+        $scope.$apply();
+      });
+    }, err => {
+      status.innerHTML = `Connection Error (Check Credentials)`;
+    });
 
   }
-$scope.select = function(key, subkey, index)
-{
-  $scope.selection =  $scope.services[key];
-  $scope.key = key;
-  $scope.subkey=subkey;
-  $scope.selectionId = index;
-  // $scope.selection_json = JSON.stringify(item,'\n',4);
-  console.log("Item:",$scope.services[key][subkey]);
-  console.log("Index:",index);
-  console.log("selection:",$scope.selection);
+  $scope.select = function(key, subkey, index) {
+    $scope.selection = $scope.services[key];
+    $scope.key = key;
+    $scope.subkey = subkey;
+    $scope.selectionId = index;
+    // $scope.selection_json = JSON.stringify(item,'\n',4);
+    console.log("Item:", $scope.services[key][subkey]);
+    console.log("Index:", index);
+    console.log("selection:", $scope.selection);
   // tableform.$setPristine();
-}
+  }
 
 
 }]);
